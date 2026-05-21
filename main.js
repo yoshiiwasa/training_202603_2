3983cbd76d7a3b319547192003b8357970f58111
Akira Tamura <aki3127377@gmail.com>
2026-05-21 16:43:09 +0900

    0521

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 'use strict';
 
 {
-
-  //選別した住所をページに表示
-
+  //0521todo
+  //関数名を適切な文字に直すこと
+  //検索結果を残す
+  //複数ある場合での表示
 
   //テキストボックスで入力された値をfetchのデータで探すのを定義
-  const showUser = async (zipcode) => {
+  //users.resultsにループ処理を追加
+  const fsearch = async (zipcode) => {
     try {
       const response = await fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcode}`);
       const users = await response.json();
+
       if (users.results !== null) {
-        const aa = users.results[0];
-        document.querySelector('p').textContent = 
-        `-- 〒${aa.zipcode} : ${aa.address1}${aa.address2}${aa.address3}(${aa.kana1}${aa.kana2}${aa.kana3}) --`;
-        console.log(users);
-      }else{
+        document.querySelector('#resultP-container').innerHTML = '';
+        users.results.forEach((result) => {
+          const newP = document.createElement('p');
+          newP.textContent = `検索結果  〒${result.zipcode} : ${result.address1}${result.address2}${result.address3}(${result.kana1}${result.kana2}${result.kana3}) `;
+          document.querySelector('#resultP-container').appendChild(newP);
+
+          const newli = document.createElement('li');
+          newli.textContent = `-- 〒${result.zipcode} : ${result.address1}${result.address2}${result.address3}(${result.kana1}${result.kana2}${result.kana3}) --`;
+          document.querySelector('#address-list').appendChild(newli);
+          // console.log(results);
+        });
+      } else {
         alert('郵便番号が存在しませんでした。再度お試しください。');
       }
     } catch (err) {
@@ -25,22 +35,26 @@
 
   //buttonを押した時のイベント(テキストボックスの値を整えてzipcodeに代入)
   //その後showUserを呼び起こす
-  document.querySelector('button').addEventListener('click', () => {
-
+  document.querySelector('#search').addEventListener('click', () => {
     const iElement = document.querySelector('input[type="text"]');
-    const zipcode = iElement.value.toLowerCase().trim().replaceAll('-', '');
+    const zipcode = iElement.value.toLowerCase().trim().replaceAll('-', '').replaceAll('ー', '').replaceAll('―', '').replaceAll('－', '');
 
     if (zipcode.length === 7) {
-      showUser(zipcode);
+      fsearch(zipcode);
     } else {
       alert('郵便番号を入力してください');
     }
   });
 
   //buttonを使いｐ要素とテキストボックスの中の文字も消す
-  document.querySelector('#preset').addEventListener('click', () => {
-  document.querySelector('p').textContent = '';
-  document.querySelector('input[type="text"]').value = '';
-});
+  document.querySelector('#resetUl').addEventListener('click', () => {
+    document.querySelector('ul').textContent = '';
+  });
+
+
+  //リセットボタンで結果も消す
+  document.querySelector('#resets').addEventListener('click', () => {
+    document.querySelector('#resultP-container').textContent = '';
+  });
 
 }
